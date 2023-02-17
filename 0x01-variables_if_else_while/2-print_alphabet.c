53fe07e177456bde840d5783ff45d4405d6f86a1
rolteame <r.s.adebiyi@gmail.com>
2023-02-17 14:56:22 -0800

    q2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Print alphabets
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+	purchar(alphabet);
+
+	return (0);
+}

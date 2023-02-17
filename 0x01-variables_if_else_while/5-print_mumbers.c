a5e6476292c5b847b895899975b3f300efd763ea
rolteame <r.s.adebiyi@gmail.com>
2023-02-17 15:52:15 -0800

    q6

---
+++ 0x01-variables_if_else_while/5-print_mumbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print all base 10 numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+
+	return (0);
+}

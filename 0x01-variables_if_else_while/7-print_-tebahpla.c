6a632610eea27e58226814ea144b9a97ad9b620a
rolteame <r.s.adebiyi@gmail.com>
2023-02-17 16:27:52 -0800

    q8

---
+++ 0x01-variables_if_else_while/7-print_-tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Print alphabets
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char lower = 'z';
+
+	while (lower <= 'a')
+	{
+		putchar(lower);
+		lower++;
+	}
+	putchar('\n');
+
+	return (0);
+}

763b3c151dfca4d6c0561a4f3ae3ed3b3e64db70
rolteame <r.s.adebiyi@gmail.com>
2023-02-21 15:16:12 -0800

    q3

---
+++ 0x02-functions_nested_loops/3-is_lower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * is_lower - check if c is lowercase
+ *
+ * Return: 1 if lower, 0 if its not
+ */
+ int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+	_putchar('\n);
+}

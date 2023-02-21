b02fcc4a0b487d48cc644ba5f414dbaea18f7cf4
rolteame <r.s.adebiyi@gmail.com>
2023-02-21 15:40:37 -0800

    q5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_sign - pring number symbol
+ *
+ * @n: number passed
+ * Return: 1 if +, 0 if 0, -1 if -
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+	}else if (n == 0)
+	{
+		return (0);
+	}else
+	{
+		return (-1)
+	}
+	_putchar('\n');
+}

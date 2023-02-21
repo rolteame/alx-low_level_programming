b2b6a0f530ed64c113940cacdd6539d8e91916a9
rolteame <r.s.adebiyi@gmail.com>
2023-02-21 12:54:36 -0800

    q2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print lowercase alphabet x10
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	while (i < 9)
+	{
+		char letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++
+		}
+		_putchar('\n')
+	}
+}

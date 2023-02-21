48aabd72ad933058d4d463cbc1b71e1eaa438d82
rolteame <r.s.adebiyi@gmail.com>
2023-02-21 12:13:43 -0800

    q1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ * print_putchar - print _putchar
+ *
+ * Return: Always 0
+ */
+
+int print_putchar (void)
+{
+	char input[9] = '_putchar';
+	int size_lenght = 9;
+	int i = 0;
+
+	while (i < size_length)
+	{
+		_putchar(input[i]);
+		i++;
+	}
+	_putchar("\n")
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,1 @@
+int print_putchar(char c)

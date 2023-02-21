a9f8223094c368196123d1991f7efa7556ae08c7
rolteame <r.s.adebiyi@gmail.com>
2023-02-21 12:45:41 -0800

    q1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - print lowercase
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter < 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+	return (0);
+}

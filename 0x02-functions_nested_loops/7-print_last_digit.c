9d657a1f4eb22a2915e66bc2c527f9880f1832ed
rolteame <r.s.adebiyi@gmail.com>
2023-02-21 16:27:09 -0800

    q7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - Print last digit
+ *
+ * @n: number
+ * Return: return last_digit
+ */
+int print_last_digit(int)
+{
+	int last_digit = n % 10;
+	return (last_digit);
+}

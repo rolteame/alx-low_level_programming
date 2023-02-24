680dfd1dc47c00ea084d00235ffb9b5894fd7033
rolteame <r.s.adebiyi@gmail.com>
2023-02-24 15:29:48 -0800

    q2&q3

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiply two nums
+ * @a: first num
+ * @b: second num
+ * Return: Always 0
+ */
+int mul(int a, int b)
+{
+	return (a*b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers using _putchar
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+}

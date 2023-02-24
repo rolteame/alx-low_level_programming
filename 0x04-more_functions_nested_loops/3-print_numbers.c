7f75b6b0bec76cf6f381179a2af9020c59999aa4
rolteame <r.s.adebiyi@gmail.com>
2023-02-24 15:48:09 -0800

    q3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,14 @@
 #include "main.h"
+
 /**
  * print_numbers - print numbers using _putchar
  * Return: Always 0
  */
 void print_numbers(void)
 {
-	int num = 0;
+	int num = 48;
 
-	while (num < 9)
+	while (num <= 57)
 	{
 		_putchar(num);
 		num++;

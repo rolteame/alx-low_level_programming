3e09abdfcfb1a71bda255635faee07f764c62b72
rolteame <r.s.adebiyi@gmail.com>
2023-02-24 15:55:13 -0800

    q4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers except 2 & 4
+ * Return: No return
+ */
+void print_most_numbers(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		if (num = 50 || num = 52)
+		{
+			continue;
+		}
+		_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+}

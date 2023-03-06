ed8c5b41301350a1c98e8ec7051e04c41462a9c8
rolteame <r.s.adebiyi@gmail.com>
2023-03-06 09:30:33 -0800

    q0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_memset - put constant byte to pointer
+ *@s: string
+ *@char: character to point
+ *@n: nmber
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		*s[i] = b;
+		i++;
+	}
+	return (s);
+}

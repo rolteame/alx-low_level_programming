7b7a0fc86bdc3841cdca50cab114249d71a874de
rolteame <r.s.adebiyi@gmail.com>
2023-03-18 05:50:54 -0700

    added new line

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,10 +9,8 @@ int main(int argc, char *argv[])
 {
 	int count;
 
-	count = 0;
-
-	for (; count < argc; count++)
-		printf("%s", argv[count]);
+	for (count = 0; count < argc; count++)
+		printf("%s\n", argv[count]);
 
 	return (0);
 }

a73b0af76ad8deebbef01a7742b57eabca9f08fe
rolteame <r.s.adebiyi@gmail.com>
2023-03-18 06:12:54 -0700

    implemented betty style

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,30 +4,39 @@
 /**
  * main - add numbers
  * @argc: count
- * argv: vector
+ * @argv: vector
  * Return: 0
  */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
 
-int main(int argc, char *argv[]) {
-    int sum = 0;
-    if (argc == 1) {
-        printf("0\n");
-        return 0;
-    }
-    for (int i = 1; i < argc; i++) {
-        int j = 0;
-        while (argv[i][j] != '\0') {
-            if (!isdigit(argv[i][j])) {
-                printf("Error\n");
-                return 1;
-            }
-            j++;
-        }
-        int num = atoi(argv[i]);
-        if (num > 0) {
-            sum += num;
-        }
-    }
-    printf("%d\n", sum);
-    return 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (int i = 1; i < argc; i++)
+	{
+		int j = 0;
+
+		while (argv[i][j] != '\0')
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		int num = atoi(argv[i]);
+
+		if (num > 0)
+		{
+			sum += num;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
 }

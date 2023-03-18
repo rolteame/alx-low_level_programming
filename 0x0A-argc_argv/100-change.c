80e530d1559b1484837bce91ada5ddd4a616b1b5
rolteame <r.s.adebiyi@gmail.com>
2023-03-18 06:16:14 -0700

    solved

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,32 +1,54 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
-/**
- * main - print coins
- * @argc: count
- * @argv: vector
- * Return: 0
- */
 
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Error\n");
-        return 1;
-    }
-    int cents = atoi(argv[1]);
-    if (cents < 0) {
-        printf("0\n");
-        return 0;
-    }
-    int num_coins = 0;
-    num_coins += cents / 25;
-    cents = cents % 25;
-    num_coins += cents / 10;
-    cents = cents % 10;
-    num_coins += cents / 5;
-    cents = cents % 5;
-    num_coins += cents / 2;
-    cents = cents % 2;
-    num_coins += cents;
-    printf("%d\n", num_coins);
-    return 0;
+/**
+ * main - Find the smallest number of coins to make an amount of change
+ * @argc: Arg count
+ * @argv: Contains all arguments
+ *
+ * Return: 1 on error, 0 for success
+ */
+int main(int argc, char *argv[])
+{	int cents, coins = 0;
+
+	if (argc == 2)
+	{
+		cents = atoi(*(argv + 1));
+		while (cents > 0)
+		{
+			if (cents % 25 < cents)
+			{
+				cents -= 25;
+				coins++;
+			}
+			else if (cents % 10 < cents)
+			{
+				cents -= 10;
+				coins++;
+			}
+			else if (cents % 5 < cents)
+			{
+				cents -= 5;
+				coins++;
+			}
+			else if (cents % 2 < cents)
+			{
+				cents -= 2;
+				coins++;
+			}
+			else if (cents % 1 < cents)
+			{
+				cents -= 1;
+				coins++;
+			}
+		}
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", coins);
+	return (0);
 }

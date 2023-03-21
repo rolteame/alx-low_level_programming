0be7720fa3e242e10f509f923520e00526e705a6
rolteame <r.s.adebiyi@gmail.com>
2023-03-20 19:28:56 -0700

    restructured header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,11 @@
  * Description: struct called "dog" that stores its name, its age
  * and the name of its owner.
  */
-struct Dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-
 };
 
 #endif

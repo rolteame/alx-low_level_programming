e19b8e6fc641d3103f35b82f670eeee4e6d4168c
rolteame <r.s.adebiyi@gmail.com>
2023-03-20 20:09:46 -0700

    restructures functions

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,10 +10,7 @@ void print_dog(struct dog *d)
 	if (d == NULL)
 		return (NULL);
 
-	if (!d->name || !d->age || !d->owner == "(nil)")
-	{
-		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
-	}
+	(!(d->name)) ? printf("Name: (nil)") : printf("Name: %s\n", d->name);
+	(!(d->age)) ? printf("Age: (nil)") : printf("Age: %f\n", d->age);
+	(!(d->owner)) ? printf("Owner: (nil)") : printf("Owner: %s\n", d->owner);
 }
